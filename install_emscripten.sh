174cb05cd42b97622fb228f213aa220e6c7dc643
SkitzFist <jo.persson@hotmail.se>
2023-09-29 21:48:19 +0200

    Altered install emscripten script

--- install_emscripten.sh
+++ install_emscripten.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if[ -d "$HOME/emssdk"]; then
-    echo "emsdk already installed"
-else
-    git clone https://github.com/emscripten-core/emsdk.git $HOME/emsdk
-    cd $HOME/emsdk
-    ./emsdk install latest
-    ./emsdk activate latest
-fi
+# Download and install Emscripten SDK
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+./emsdk install latest
+./emsdk activate latest
 
-source $HOME/emsdk/emsdk_env.sh
+# Make emsdk commands available for the current session
+source ./emsdk_env.sh
