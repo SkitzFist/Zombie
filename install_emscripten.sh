bbe4e1640f2c77df40e38596b2c47aa43903e8e3
SkitzFist <jo.persson@hotmail.se>
2023-09-30 17:43:11 +0200

    Updated install script

--- install_emscripten.sh
+++ install_emscripten.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Download and extract xz utility
+curl -L -o xz.tar.gz https://tukaani.org/xz/xz-5.2.5-linux.tar.gz
+tar -xzf xz.tar.gz
+# List the contents of the directory
+ls -la $(pwd)/xz-5.2.5-linux/bin
+# Give execution permissions to the binaries
+chmod +x $(pwd)/xz-5.2.5-linux/bin/*
+export PATH=$(pwd)/xz-5.2.5-linux/bin:$PATH
+
 # Download and install Emscripten SDK
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
