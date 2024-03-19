bea0a2046a414ee03c67511eb3fadf9c5293d5ff
SkitzFist <jo.persson@hotmail.se>
2024-03-19 20:43:25 +0100

    More aggressive compile/link optimizations & updated MS, TP

--- src/MoveSystem.h
+++ src/MoveSystem.h
@@ -1,37 +1,71 @@
 #ifndef ZOMBIE_MOVE_SYSTEM_H_
 #define ZOMBIE_MOVE_SYSTEM_H_
 
-#include "raylib.h"
-#include "ThreadPool.h"
 #include "PositionComponent.h"
 #include "SpeedComponent.h"
+#include "ThreadPool.h"
+#include "raylib.h"
 
-inline void move(int startIndex, int length, PositionComponent &positions, SpeedComponent& speeds,const float dt) {
+// debug
+#include "Log.hpp"
+
+inline void moveX(int startIndex, int length, PositionComponent &positions, SpeedComponent &speeds) {
     for (int i = startIndex; i < (startIndex + length); ++i) {
-          int entityId = i;
+        positions.xPos[i] += speeds.velX[i];
+    }
+}
 
-          speeds.velX[entityId] += speeds.accX[entityId];
-          speeds.velY[entityId] += speeds.accY[entityId];
+inline void moveY(int startIndex, int length, PositionComponent &positions, SpeedComponent &speeds) {
+    for (int i = startIndex; i < (startIndex + length); ++i) {
+        positions.yPos[i] += speeds.velY[i];
+    }
+}
 
-          positions.xPos[entityId] += speeds.velX[entityId];
-          positions.yPos[entityId] += speeds.velY[entityId];
+inline void updateVelX(int startIndex, int length, SpeedComponent &speeds) {
+    for (int i = startIndex; i < (startIndex + length); ++i) {
+        speeds.velX[i] += speeds.accX[i];
+    }
+}
+
+inline void updateVelY(int startIndex, int length, SpeedComponent &speeds) {
+    for (int i = startIndex; i < (startIndex + length); ++i) {
+        speeds.velY[i] += speeds.accY[i];
     }
 }
 
 struct MoveSystem {
     bool isActive = false;
 
-    void update(PositionComponent &positions, SpeedComponent& speeds, ThreadPool& threadPool) {
+    void update(PositionComponent &positions, SpeedComponent &speeds, ThreadPool &threadPool) {
         if (!isActive) {
             return;
         }
 
-        int numberOfThreads = 4;
+
+        int numberOfThreads = 2;
         int length = positions.size / numberOfThreads;
         int startIndex = 0;
 
         for (int i = 0; i < numberOfThreads; ++i) {
-            threadPool.enqueue(move, startIndex, length, std::ref(positions), std::ref(speeds), GetFrameTime());
+            threadPool.enqueue(updateVelX, startIndex, length, std::ref(speeds));
+            startIndex += length;
+        }
+
+        startIndex = 0;
+        for (int i = 0; i < numberOfThreads; ++i) {
+            threadPool.enqueue(updateVelY, startIndex, length, std::ref(speeds));
+            startIndex += length;
+        }
+
+        startIndex = 0;
+        for (int i = 0; i < numberOfThreads; ++i) {
+            threadPool.enqueue(moveX, startIndex, length, std::ref(positions), std::ref(speeds));
+            startIndex += length;
+        }
+
+        startIndex = 0;
+        for (int i = 0; i < numberOfThreads; ++i) {
+            threadPool.enqueue(moveY, startIndex, length, std::ref(positions), std::ref(speeds));
             startIndex += length;
         }
     }
--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -2,11 +2,11 @@
 #define ZOMBIE_THREAD_POOL_H_
 
 #include <functional>
+#include <future>
 #include <iostream>
 #include <pthread.h>
 #include <queue>
 #include <vector>
-#include <future>
 
 class ThreadPool {
   public:
@@ -14,14 +14,18 @@ class ThreadPool {
     ~ThreadPool();
 
     template <class F, class... Args>
-    void enqueue(F &&f, Args &&...args);
+    auto enqueue(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>;
+
+    void awaitCompletion();
 
   private:
     std::vector<pthread_t> workers;
     std::queue<std::function<void()>> tasks;
+    size_t tasks_in_progress = 0;
 
     pthread_mutex_t queue_mutex;
     pthread_cond_t condition;
+    pthread_cond_t completion_condition;
     bool stop;
 
     static void *worker(void *arg);
@@ -30,6 +34,7 @@ class ThreadPool {
 inline ThreadPool::ThreadPool(size_t threads) : stop(false) {
     pthread_mutex_init(&queue_mutex, nullptr);
     pthread_cond_init(&condition, nullptr);
+    pthread_cond_init(&completion_condition, nullptr);
 
     for (size_t i = 0; i < threads; ++i) {
         pthread_t worker_thread;
@@ -55,19 +60,34 @@ inline ThreadPool::~ThreadPool() {
 
     pthread_mutex_destroy(&queue_mutex);
     pthread_cond_destroy(&condition);
+    pthread_cond_destroy(&completion_condition);
 }
 
 template <class F, class... Args>
-inline void ThreadPool::enqueue(F &&f, Args &&...args) {
-    auto task = std::make_shared<std::packaged_task<void()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+inline auto ThreadPool::enqueue(F &&f, Args &&...args)->std::future<typename std::result_of<F(Args...)>::type> {
+    using return_type = typename std::result_of<F(Args...)>::type;
 
+    auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    std::future<return_type> res = task->get_future();
     pthread_mutex_lock(&queue_mutex);
     if (stop) {
         pthread_mutex_unlock(&queue_mutex);
         throw std::runtime_error("enqueue on stopped ThreadPool");
     }
     tasks.emplace([task]() { (*task)(); });
+    tasks_in_progress++;
     pthread_cond_signal(&condition);
+    pthread_mutex_unlock(&queue_mutex);
+
+    return res;
+}
+
+inline void ThreadPool::awaitCompletion() {
+    pthread_mutex_lock(&queue_mutex);
+    while (!tasks.empty() || tasks_in_progress > 0) {
+        pthread_cond_wait(&completion_condition, &queue_mutex);
+    }
     pthread_mutex_unlock(&queue_mutex);
 }
 
@@ -89,6 +109,12 @@ inline void *ThreadPool::worker(void *arg) {
         pthread_mutex_unlock(&pool->queue_mutex);
 
         task();
+        pthread_mutex_lock(&pool->queue_mutex);
+        pool->tasks_in_progress--;
+        if (pool->tasks_in_progress == 0 && pool->tasks.empty()) {
+            pthread_cond_signal(&pool->completion_condition);
+        }
+        pthread_mutex_unlock(&pool->queue_mutex);
     }
     return nullptr;
 }
