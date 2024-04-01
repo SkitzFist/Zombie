1d301baec3aa5eec203fb744c6ae656acf159183
SkitzFist <jo.persson@hotmail.se>
2024-04-01 13:35:37 +0200

    Use SSE 4.1 for move system

--- src/MoveSystem.h
+++ src/MoveSystem.h
@@ -1,24 +1,45 @@
 #ifndef ZOMBIE_MOVE_SYSTEM_H_
 #define ZOMBIE_MOVE_SYSTEM_H_
 
+#include <smmintrin.h>
+
+#include "MathHack.hpp"
 #include "PositionComponent.h"
 #include "SpeedComponent.h"
 #include "ThreadPool.h"
 #include "raylib.h"
-#include "MathHack.hpp"
 
 // debug
 #include "Log.hpp"
 
 inline void moveX(int startIndex, int length, PositionComponent &positions, SpeedComponent &speeds) {
-    for (int i = startIndex; i < (startIndex + length); ++i) {
+    int i = startIndex;
+    for (; i + 3 < (startIndex + length); i += 4) {
+        __m128 xPos = _mm_loadu_ps(&positions.xPos[i]);
+        __m128 velX = _mm_loadu_ps(&speeds.velX[i]);
+
+        __m128 result = _mm_add_ps(xPos, velX);
+
+        _mm_storeu_ps(&positions.xPos[i], result);
+    }
+
+    for (; i < (startIndex + length); ++i) {
         positions.xPos[i] += speeds.velX[i];
-        //positions.yPos[i] += speeds.velY[i];
     }
 }
 
 inline void moveY(int startIndex, int length, PositionComponent &positions, SpeedComponent &speeds) {
-    for (int i = startIndex; i < (startIndex + length); ++i) {
+    int i = startIndex;
+    for (; i + 3 < (startIndex + length); i += 4) {
+        __m128 yPos = _mm_loadu_ps(&positions.yPos[i]);
+        __m128 velY = _mm_loadu_ps(&speeds.velY[i]);
+
+        __m128 result = _mm_add_ps(yPos, velY);
+
+        _mm_storeu_ps(&positions.yPos[i], result);
+    }
+
+    for (; i < (startIndex + length); ++i) {
         positions.yPos[i] += speeds.velY[i];
     }
 }
@@ -26,17 +47,48 @@ inline void moveY(int startIndex, int length, PositionComponent &positions, Spee
 inline constexpr const float MAX_VEL = 5.f;
 
 inline void updateVelX(int startIndex, int length, SpeedComponent &speeds) {
-    
-    for (int i = startIndex; i < (startIndex + length); ++i) {
+    // Define max and min velocity as __m128 vectors
+    __m128 maxVel = _mm_set1_ps(MAX_VEL);
+    __m128 minVel = _mm_set1_ps(-MAX_VEL);
+
+    int i = startIndex;
+    // Process in chunks of 4 floats since SSE can handle 4 floats at a time
+    for (; i + 3 < (startIndex + length); i += 4) {
+        // Load 4 velocity and acceleration elements from the arrays
+        __m128 velX = _mm_loadu_ps(&speeds.velX[i]);
+        __m128 accX = _mm_loadu_ps(&speeds.accX[i]);
+
+        // Add acceleration to velocity
+        __m128 updatedVelX = _mm_add_ps(velX, accX);
+
+        // Clamp the updated velocities to the max and min values
+        updatedVelX = _mm_max_ps(minVel, _mm_min_ps(maxVel, updatedVelX));
+
+        // Store the clamped velocities back into the array
+        _mm_storeu_ps(&speeds.velX[i], updatedVelX);
+    }
+
+    // Process any remaining elements that don't fit into a vector of 4
+    for (; i < (startIndex + length); ++i) {
         speeds.velX[i] += speeds.accX[i];
         speeds.velX[i] = MathHack::clamp(speeds.velX[i], -MAX_VEL, MAX_VEL);
     }
 }
 
 inline void updateVelY(int startIndex, int length, SpeedComponent &speeds) {
-    for (int i = startIndex; i < (startIndex + length); ++i) {
-        speeds.velY[i] += speeds.accY[i];
-        speeds.velY[i] = MathHack::clamp(speeds.velY[i], -MAX_VEL, MAX_VEL);
+    __m128 maxVel = _mm_set1_ps(MAX_VEL);
+    __m128 minVel = _mm_set1_ps(-MAX_VEL);
+
+    int i = startIndex;
+    for (; i + 3 < (startIndex + length); i += 3) {
+        __m128 velY = _mm_loadu_ps(&speeds.velY[i]);
+        __m128 accY = _mm_loadu_ps(&speeds.accY[i]);
+
+        __m128 updatedVelY = _mm_add_ps(velY, accY);
+
+        updatedVelY = _mm_max_ps(minVel, _mm_min_ps(maxVel, updatedVelY));
+
+        _mm_storeu_ps(&speeds.velY[i], updatedVelY);
     }
 }
 
@@ -47,7 +99,6 @@ struct MoveSystem {
         if (!isEnabled) {
             return;
         }
-
 
         int numberOfThreads = 4;
         int length = positions.size / numberOfThreads;
